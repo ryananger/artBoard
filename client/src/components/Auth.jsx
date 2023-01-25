30c3206ec55d4297ed73bbeedbfe5b5894a19916
ryananger <nightcrawling@gmail.com>
2023-01-25 15:41:22 -0500

    Firebase signup and styles.

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -8,17 +8,28 @@ import cookieHandle from '../util/cookieHandle.js';
 const Auth = function({state}) {
   const [signUp, setSignUp] = useState(false);
 
+  var handleSubmit = function(e) {
+    e.preventDefault();
+
+    var form = e.target;
+
+    if (signUp) {
+      auth.signUp(form.email.value, form.pass.value);
+    } else {
+      auth.signIn(form.email.value, form.pass.value);
+    }
+  };
 
   var renderLogin = function() {
     return (
-      <form id='loginForm' className='loginForm v'>
+      <form id='loginForm' className='loginForm v' onSubmit={handleSubmit}>
         <div className='loginInputs v'>
-          <input className='loginInput' type='text' placeholder='Email address!'/>
-          <input className='loginInput' type='text' placeholder='Password!'/>
+          <input className='loginInput' name='email' type='text' placeholder='Email address!'/>
+          <input className='loginInput' name='pass'  type='text' placeholder='Password!'/>
         </div>
         <div className='loginButtons h'>
-          <input className='loginButton' type='submit' value='sign up'/>
-          <input className='loginButton' type='submit' value='sign in'/>
+          <input className='loginButton' type='submit' value='sign up' onClick={()=>{setSignUp(true)}}/>
+          <input className='loginButton' type='submit' value='sign in' onClick={()=>{setSignUp(false)}}/>
         </div>
       </form>
     )
